d1fed9c75d4882563a1375596ebabf5f55160c01
jim <jim@acceleration.net>
2017-01-04 18:42:03 -0500

    Ensure a valid element was added before trying to apply a class to it.

--- src/findAndReplaceDOMText.js
+++ src/findAndReplaceDOMText.js
@@ -498,7 +498,7 @@
 
 			var el = typeof wrapper == 'string' ? doc.createElement(wrapper) : wrapper;
 
- 			if (wrapperClass) {
+ 			if (wrapperClass && el !== undefined) {
 				el.className = wrapperClass;
 			}
 
